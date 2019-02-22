f9bec4cdc1cf623d10efb2b34832c9d7d2b2be77
shikky0331 <shikky0335@gmail.com>
2019-02-22 11:23:54 +0900

    model&view ref

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,6 @@ class Message < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :asc) }
 
-  # validates :user_id, presence: true
   validates :content, presence: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # ユーザーから申請したユーザーとのfriendships情報を表示
   has_many :friendships_of_from_user, :class_name => 'Friendship', :foreign_key => 'from_user_id', :dependent => :destroy
-# ユーザーから申請したユーザーとのfriendships情報を表示
 
+  # ユーザーが申請されたユーザーとのfriendships情報表示
   has_many :friendships_of_to_user, :class_name => 'Friendship', :foreign_key => 'to_user_id', :dependent => :destroy
-# ユーザーが申請されたユーザーとのfriendships情報表示
 
-  has_many :friends_of_from_user, :through => :friendships_of_from_user, :source => 'to_user'
   # ユーザーから申請したユーザーの情報を表示
+  has_many :friends_of_from_user, :through => :friendships_of_from_user, :source => 'to_user'
 
-  has_many :friends_of_to_user, :through => :friendships_of_to_user, :source => 'from_user'
   # ユーザーが申請されたユーザーの情報を表示
+  has_many :friends_of_to_user, :through => :friendships_of_to_user, :source => 'from_user'
 
   has_many :messages
 
@@ -20,10 +21,6 @@ class User < ActiveRecord::Base
 
   def friends
      friends_of_from_user + friends_of_to_user
-  end
-
-  def unfollow!(id)
-    friendships_of_from_user.find_by(to_user_id: id).destroy
   end
 
 end
