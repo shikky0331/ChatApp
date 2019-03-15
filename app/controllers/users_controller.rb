0162e841fcd44d4d35082272acb986def3d81afc
shikky0331 <shikky0335@gmail.com>
2019-03-15 22:52:19 +0900

    argument of WHERE must be type boolean, not type integer　改善

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(current_user.id)
+    @user = User.find_by(id: current_user.id)
   end
 
   def edit
-    @user = User.find_by(current_user.id)
+    @user = User.find_by(id: current_user.id)
   end
 
 end
